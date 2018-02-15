827405160a4e5b71ff254cda4383f5977fa80b6f
Jason <jgroff88@gmail.com>
2018-02-14 18:11:32 -0600

    added buttons and JS functionality

---
+++ myNameButton/myName.js
@@ -0,0 +1,11 @@
+function myNameAlert() {
+    alert("My name is Jason!");
+}
+
+function printNameConsole() {
+    console.log("Hello Jason!");
+}
+
+function printNameDocument() {
+    document.write("Hello Jason!");
+}
