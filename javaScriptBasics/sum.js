29be6bb0606d0894e6c57039b9d3a096f710e550
Jason Groff <jgroff88@gmail.com>
2018-02-22 21:23:47 -0600

    added event listener

---
+++ javaScriptBasics/sum.js
@@ -0,0 +1,5 @@
+function sum() {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  alert("The answer is " + num1 + num2 + " !");
+}
