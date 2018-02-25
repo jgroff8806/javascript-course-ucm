895367a7ac8f8d03980ae649462c56626b9dd604
Jason <jgroff88@gmail.com>
2018-02-25 17:42:46 -0600

    finished Assignment #4

---
+++ radiusCircle/app.js
@@ -0,0 +1,13 @@
+// Calculate area of circle function
+function area() {
+  var radius = parseInt(document.getElementById("radius").value);
+  var areaCalc = Math.PI * radius * radius;
+  alert("The area of the circle is " + areaCalc.toFixed(2));
+}
+
+// Calculate circumference of circle function
+function circumference() {
+  var radius = parseInt(document.getElementById("radius").value);
+  var circCalc = 2 * radius * Math.PI;
+  alert("The circumference of the circle is " + circCalc.toFixed(2));
+}
