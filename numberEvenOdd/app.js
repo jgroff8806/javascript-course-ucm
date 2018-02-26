1e7d4935edf40e4f7c7174d8f17785135b760af6
Jason <jgroff88@gmail.com>
2018-02-25 18:04:31 -0600

    Finished Assignment #5 (Bonus)

---
+++ numberEvenOdd/app.js
@@ -0,0 +1,9 @@
+// Function evaluates whether a value input by the user is even or odd
+function evalNum() {
+  var num = parseInt(document.getElementById("num").value);
+  if (num % 2 == 0) {
+    alert("The number you entered is Even");
+  } else {
+    alert("The number you entered is Odd");
+  }
+}
