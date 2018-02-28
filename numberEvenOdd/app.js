f75269fe30067dcc08fe4bd5ba79275bde26ade5
Jason <jgroff88@gmail.com>
2018-02-28 16:13:07 -0600

    Week 2 Day 2: Assignment #5 Even or Odd Number - Finished

--- numberEvenOdd/app.js
+++ numberEvenOdd/app.js
@@ -1,3 +1,8 @@
+/*
+  Create a function to evaluate whether a given input is even or odd
+  Output the result 
+*/
+
 // Function evaluates whether a value input by the user is even or odd
 function evalNum() {
   var num = parseInt(document.getElementById("num").value);
