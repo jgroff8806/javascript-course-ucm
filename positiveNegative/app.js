0bcca117fef3f3537489df60550b712b9c11935a
Jason <jgroff88@gmail.com>
2018-02-28 14:08:07 -0600

    TypeError

--- positiveNegative/app.js
+++ positiveNegative/app.js
@@ -1,3 +1,7 @@
+// posNeg Exercise
+// Given 2 int values, return true if one is negative and one is positive.
+// Except if the parameter "negative" is true, then return true only if both are negative.
+
 function evalPosNeg(num1, num2, negative) {
   if (negative && num1 < 0 && num2 < 0) {
     return true;
