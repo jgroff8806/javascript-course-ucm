3817dce46d8dee00070fc902bf00bccf36290979
Jason <jgroff88@gmail.com>
2018-02-28 14:59:58 -0600

    incomplete solution

--- positiveNegative/app.js
+++ positiveNegative/app.js
@@ -2,6 +2,9 @@
 // Given 2 int values, return true if one is negative and one is positive.
 // Except if the parameter "negative" is true, then return true only if both are negative.
 
+var num1 = parseInt(document.getElementById("num1").value);
+var num2 = parseInt(document.getElementById("num2").value);
+
 function evalPosNeg(num1, num2) {
   // return true if one value is negative and one is positive
   if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
@@ -9,12 +12,12 @@ function evalPosNeg(num1, num2) {
     // return true if both are negative
   } else if (num1 < 0 && num2 < 0) {
     return true;
-    // return false if neither are
   } else {
     return false;
   }
 }
 
-/*
-  document.getElementById("evalButton").addEventListener("click", evalPosNeg()).innerHTML = "evalPosNeg()";
-*/
+document
+  .getElementById("evalButton")
+  .addEventListener("click", evalPosNeg()).innerHTML =
+  "evalPosNeg()";
