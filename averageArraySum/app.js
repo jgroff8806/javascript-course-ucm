33faa2720971a3c1e71e0a3530827bf8a9e53c69
Jason <jgroff88@gmail.com>
2018-02-28 18:18:37 -0600

    incomplete solution

--- averageArraySum/app.js
+++ averageArraySum/app.js
@@ -1,3 +1,8 @@
+/* 
+  Write a JavaScript program that sums all elements in array 
+  Once you have the sum FIND the average of the sum
+*/
+
 var sum = [1, 2, 5, 15, 25, 50].reduce(add, 0);
 
 function add(a, b) {
