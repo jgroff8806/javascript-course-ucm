bbb7b7eeb31500efd144ca0be7023037d8e55f02
Jason <jgroff88@gmail.com>
2018-02-28 22:56:23 -0600

    Week 3 Day 2: Assignment #2 Positive Negative - Finished

--- positiveNegative/app.js
+++ positiveNegative/app.js
@@ -1,23 +1,21 @@
 // posNeg Exercise
-// Given 2 int values, return true if one is negative and one is positive.
-// Except if the parameter "negative" is true, then return true only if both are negative.
+// Given 2 int values, return true if one is negative and one is positive
+// Except if the parameter "negative" is true, then return true only if both are negative
 
-var num1 = parseInt(document.getElementById("num1").value);
-var num2 = parseInt(document.getElementById("num2").value);
+function evalPosNeg(num1, num2, negative) {
+  // return true if the parameter 'negative' is true
+  // and both number parameters are negative
+  if (negative === true && num1 < 0 && num2 < 0) {
+    console.log("true");
+    return true;
+  }
 
-function evalPosNeg(num1, num2) {
   // return true if one value is negative and one is positive
   if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
+    console.log("true");
     return true;
-    // return true if both are negative
-  } else if (num1 < 0 && num2 < 0) {
-    return true;
-  } else {
-    return false;
   }
-}
 
-document
-  .getElementById("evalButton")
-  .addEventListener("click", evalPosNeg()).innerHTML =
-  "evalPosNeg()";
+  console.log("false");
+  return false;
+}
