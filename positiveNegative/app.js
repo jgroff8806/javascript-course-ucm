27011644ca12932161b46acf71ceeb34d4802b14
Jason <jgroff88@gmail.com>
2018-02-28 23:11:56 -0600

    Week 3 Day 1: Assignment #2 Positive Negative - Finished

--- positiveNegative/app.js
+++ positiveNegative/app.js
@@ -1,4 +1,4 @@
-// posNeg Exercise
+// Positive Negative Exercise
 // Given 2 int values, return true if one is negative and one is positive
 // Except if the parameter "negative" is true, then return true only if both are negative
 
