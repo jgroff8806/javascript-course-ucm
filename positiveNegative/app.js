3669e643344d8a034e067b5602c75f52e022ae31
Sean <sgroff04@gmail.com>
2018-03-04 14:31:44 -0600

    Finished adding form and styling

--- positiveNegative/app.js
+++ positiveNegative/app.js
@@ -2,23 +2,31 @@
 // Given 2 values, return true if one is negative and one is positive
 // Except if the parameter "negative" is true, then return true only if both are negative
 
-function evalPosNeg(num1, num2, negative) {
+var inputNumOne = document.getElementById('num1');
+var inputNumTwo = document.getElementById('num2');
+var negativeCheck = document.getElementById('negativeCheck');
+var answer = document.getElementById('answer');
+
+function evalPosNeg(e) {
+  e.preventDefault();
+
+  var num1 = inputNumOne.value;
+  var num2 = inputNumTwo.value;
+  var isNegativeChecked = negativeCheck.checked;
+
   // return true if the parameter 'negative' is true
   // and both number parameters are negative
-  if (negative === true && num1 < 0 && num2 < 0) {
-    console.log("true");
+  if (isNegativeChecked === true && num1 < 0 && num2 < 0) {
+    answer.innerHTML = 'True';
     return true;
   }
 
   // return true if one value is negative and one is positive
   if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
-    console.log("true");
+    answer.innerHTML = 'True';
     return true;
   }
 
-  console.log("false");
+  answer.innerHTML = 'False';
   return false;
 }
-
-// create a function to change
-var funButton = function() {};
