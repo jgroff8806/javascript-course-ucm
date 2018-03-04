f43e0db45fcea5eeac3af7fcb6b840ebeec9c1f9
Jason <jgroff88@gmail.com>
2018-03-04 16:10:48 -0600

    Week 3 Day 2: Assignment #2 Sum Double - Finished

--- sumDouble/app.js
+++ sumDouble/app.js
@@ -1,14 +1,25 @@
 // Given two int values, return their sum
 // If the two values are the same, return double their sum
 
-function sumDouble(num1, num2) {
+var inputNumOne = document.getElementById("num1");
+var inputNumTwo = document.getElementById("num2");
+var answer = document.getElementById("answer");
+var sumAnswer = document.getElementById("sumAnswer");
+var sumAnswerDouble = document.getElementById("sumAnswerDouble");
+
+function sumDouble(e) {
+  e.preventDefault();
+
+  var num1 = inputNumOne.value;
+  var num2 = inputNumTwo.value;
+  var sumAnswer = +num1 + +num2;
+  var sumAnswerDouble = 2 * (+num1 + +num2);
+
   if (num1 === num2) {
-    console.log(2 * (num1 + num2));
-    return 2 * (num1 + num2);
+    answer.innerHTML = sumAnswerDouble;
+    return sumAnswerDouble;
   } else {
-    console.log(num1 + num2);
-    return num1 + num2;
+    answer.innerHTML = sumAnswer;
+    return sumAnswer;
   }
 }
-
-// sumDouble(2, 2);
