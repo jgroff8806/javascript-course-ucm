bd90d170fbefd330f2d9b12326f9b523b79a4e84
Jason <jgroff88@gmail.com>
2018-03-04 22:20:56 -0600

    Week 3 Day 2: Assignment #4 Temperature Evaluation - Finished

--- temperatureEval/app.js
+++ temperatureEval/app.js
@@ -4,12 +4,22 @@
   Return false if one is greater than 0 and the other is greater than 100
 */
 
-function tempEval(temp1, temp2) {
+var inputTempOne = document.getElementById("temp1");
+var inputTempTwo = document.getElementById("temp2");
+var result = document.getElementById("result");
+
+function tempEval(e) {
+  e.preventDefault();
+
+  var temp1 = inputTempOne.value;
+  var temp2 = inputTempTwo.value;
+  // Return true if one temperature value is less than 0 and the other is greater than 100
   if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
-    console.log("true");
+    result.innerHTML = "True";
     return true;
+    // Return false if one temperature value is greater than 0 and the other is greater than 100
   } else if ((temp1 > 0 && temp2 > 100) || (temp1 > 100 && temp2 > 0)) {
-    console.log("false");
+    result.innerHTML = "False";
     return false;
   }
 }
