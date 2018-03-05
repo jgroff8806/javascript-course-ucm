a10aaf8a2e4f55c9102194ece5865a087702d28a
Jason <jgroff88@gmail.com>
2018-03-04 20:37:30 -0600

    Week 3 Day 2: Assignment #3 Print Odd Numbers - Finished

--- printOddNumbers/app.js
+++ printOddNumbers/app.js
@@ -2,8 +2,20 @@
   Write a program to display all odd numbers from 1 to 100
 */
 
+var res = document.getElementById("result");
+
 for (var i = 0; i < 100; i++) {
+  // Create paragraph element
+  var newPara = document.createElement("p");
+
   if (i % 2 !== 0) {
-    document.getElementById("result").innerHTML += i + "<br>";
+    // Stores number
+    var number = document.createTextNode(i);
+    // Append number to new paragraph element
+    newPara.appendChild(number);
+    // Adds CSS class to new paragraph element
+    newPara.classList.add("number");
+    // Append new paragraph element to results div
+    res.appendChild(newPara);
   }
 }
