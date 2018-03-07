ff4e79497eca76ab367c1a2017722bb81cd15fa9
Jason Groff <jgroff88@gmail.com>
2018-03-06 20:59:26 -0600

    Week 4 Day 1: JavaScript DOM Exercises - Exercise 1 - Finished

---
+++ javascriptDOMExercises/exercise1/app.js
@@ -0,0 +1,8 @@
+// Function changes the font style, font size and color
+// of the <p> element "text"
+
+var js_style = function() {
+  text.style.fontFamily = "Arial";
+  text.style.fontSize = "20px";
+  text.style.color = "blue";
+};
