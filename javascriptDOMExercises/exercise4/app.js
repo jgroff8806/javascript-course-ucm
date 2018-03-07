dff2da875f8e13b98dc08ac798e14b53ec1dab73
Jason <jgroff88@gmail.com>
2018-03-07 15:56:45 -0600

    Week 4 Day 1: JavaScript DOM Exercises - Exercise 4 - Finished

--- javascriptDOMExercises/exercise4/app.js
+++ javascriptDOMExercises/exercise4/app.js
@@ -1,12 +1,16 @@
 var getAttributes = function() {
   // Selects the <a> element and returns the id "w3r" and stores it in link variable
   var link = document.getElementById("w3r");
+
+  // link is then used to reference each attribute available from the <a> element "w3r"
+  // and stores them in their own respective variable
   var href = link.href;
   var hreflang = link.hreflang;
   var rel = link.rel;
   var target = link.target;
   var type = link.type;
 
+  // Console.log includes concatination and use of line breaks "\n" to seperate each attribute value
   console.log(
     "href: " +
       href +
