15d555ccd52deb2fdc56156c3fd6076c26a11838
Jason Groff <jgroff88@gmail.com>
2018-03-13 19:27:37 -0500

    review try/catch/finally

---
+++ inClassExamples/errorHandling/app.js
@@ -0,0 +1,25 @@
+// Call the function
+console.log(displayMonthName());
+
+function getMonthName(monthNumber) {
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  if (months[monthNumber - 1]) {
+    return months[monthNumber - 1];
+  } else {
+    throw "Invalid number passed";
+  }
+}
+
+function displayMonthName() {
+  try {
+    var monthName = getMonthName(12);
+    console.log(monthName);
+  } catch (e) {
+    monthName = "unknown";
+    console.log(monthName);
+    console.log("The number you passed is invalid please try again.");
+  } finally {
+    console.log("I am going to eat my granola bar");
+  }
+}
