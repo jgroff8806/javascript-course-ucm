baea28853b6edde98e91a57beffe32649979eef5
Jason Groff <jgroff88@gmail.com>
2018-03-13 21:10:20 -0500

    review JS

--- calculatorApp/script.js
+++ calculatorApp/script.js
@@ -1,70 +1,117 @@
-// get two inputs by id
-// get operator id, inputOp
-// operand vars (2 of them)
-var textInput1 = document.getElementById("txtInput1");
-var textInput2 = document.getElementById("txtInput2");
-var inputOperator = document.getElementById("txtOperator");
-var operand1;
-var operand2;
-
-/* 
-Functions to create:
-  del()
-  reset()
-  onClick(event)
-  isOperator()
-  onDecimal()
-  calculate()
+/*Function to give input. 
+* @param evt: Will have event information. 
+* This will help us findout which button is clicked 
 */
-
-// Function to give input
-// @param evt: Will have event information
-// This will help us find out which button is clicked
-// onclick function(event)
-
 function onClick(evt) {
-  var char = event.target.innerText;
+  var char = evt.target.innerText;
+  //if what user entered is an operator
   if (isOperator(char)) {
+    //if first input exist
     if (input1.value) {
+      //assign input1.value to var operand1
       operand1 = input1.value;
-      inputOperator.value = char;
+      //assign inputOp.value to char
+      inputOp.value = char;
     }
   } else {
+    //if user already entered the first operand
     if (operand1) {
+      //then update then keep adding to the
+      //second input with whatever with value user entered
       input2.value = input2.value + char;
     } else {
+      //else update
       input1.value = input1.value + char;
     }
   }
 }
 
-// isoperator function, to know whether user has click on operator buttons (+ - * / =)
-function isOperator() {
-  // list each operator "+, -, *, /, =, del, res"
+/**
+ *
+ * @param char: To know whether user has clicked on operator buttons such as +,-,* and /
+ */
+function isOperator(char) {
+  if (char === "+" || char == "-" || char == "*" || char == "/") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// reset function to empty or reset the calculator
+/**
+ * Click event handler for button "C". This resets the calculator
+ */
 function reset() {
-  // this function reset
+  input1.value = "";
+  input2.value = "";
+  operand1 = null;
+  operand2 = null;
+  inputOp.value = "";
 }
 
-// delete function, works like backspace
+/** Event handler for delete key. works like a backspace*/
 function del() {
-  //
+  if (!operand1) {
+    var val = input1.value;
+    input1.value = removeChar(val);
+  } else {
+    var val = input2.value;
+    input2.value = removeChar(val);
+  }
 }
 
-// function to remove character (for delete function)
+//function to handle dot(.) button. Only one decimal is allowed
+function onDecimal() {
+  if (operand1) {
+    var val = input2.value;
+    if (val.indexOf(".") > -1) return;
+    input2.value += ".";
+  } else {
+    var val = input1.value;
+    if (val.indexOf(".") > -1) return;
+    input1.value += ".";
+  }
+}
 
-// function for calculate() to calculate input numbers and operators
+function removeChar(val) {
+  if (val) {
+    //if more than one number exists then
+    //return everything between 0th and last but one - exclusive
+    return val.substring(0, val.length - 1);
+  }
+  return "";
+}
+
+//calculates the result when user clicks on (=) button
 function calculate() {
-  //add
-  //subtract
-  //multiply
-  //divide
+  if (operand1 && input2.value) {
+    var op1 = +operand1;
+    var op2 = +input2.value;
+    var op = inputOp.value;
+
+    reset();
+    input1.value = calculateResult(op1, op2, op);
+  }
 }
 
-// error handling for character
-try {
-} catch {
-} finally {
+function calculateResult(op1, op2, operator) {
+  switch (operator) {
+    case "+":
+      return op1 + op2;
+    case "-":
+      return op1 - op2;
+    case "*":
+      return op1 * op2;
+    case "/":
+      if (op2 == 0) {
+        alert("Can not divide by zero");
+        return 0;
+      }
+      var result = op1 / op2;
+      if (Number.isInteger(result)) return result;
+      return result.toFixed(2);
+
+    default:
+      return 0;
+  }
 }
