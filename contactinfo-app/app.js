f421028eb257bfadcbaa2f8dff127c2ed0c04636
Jason Groff <jgroff88@gmail.com>
2018-03-15 20:15:10 -0500

    changed form

--- contactinfo-app/app.js
+++ contactinfo-app/app.js
@@ -6,6 +6,17 @@ var app = express();
 // Run this app on port 3000
 var port = 3000;
 
+// Create a variable called mongoose
+// Notify the app that it needs mongoose framework
+var mongoose = require("mongoose");
+// Connect the app to the db in the specified location
+mongoose.connect("mongodb://localhost:27017/contact-app");
+
+var contactSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+});
+
 // Binding root context that is localhost:3000
 // to get method that responds with index.html file
 // req - HTTP request made by client
@@ -14,4 +25,9 @@ app.get("/", (req, res) => {
   // sendFile is a method in express framework that sends file back
   // to the client
   res.sendFile(__dirname + "/index.html");
+});
+
+// Informing your app to listen to port number you have provided on top
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
 });
