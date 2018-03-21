50df57dbf42c3f795ef92339bddcd65058517a97
Jason Groff <jgroff88@gmail.com>
2018-03-20 20:25:38 -0500

    added ejs and views, deleted index.html

--- compact-contact-form/app.js
+++ compact-contact-form/app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var port = 3000;
 
+app.set("view engine", "ejs");
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(
@@ -14,18 +16,21 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/compact-contact-form");
 
+// Inform the app use ejs as its view engine
+app.set("view engine", "ejs");
+
 var compactContactSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
-  subject: [],
+  subject: String,
   message: String,
   created_at: { type: Date, default: Date.now() }
 });
 var CompactContact = mongoose.model("CompactContactApp", compactContactSchema);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
 });
 
 // Get request for style.css file
