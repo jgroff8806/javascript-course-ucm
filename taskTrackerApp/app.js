01639d032843a6041e853a80978b4557b44076df
dwsnapit <36215743+dwsnapit@users.noreply.github.com>
2018-04-03 18:16:42 -0500

    moving App.use for connecting to db

--- taskTrackerApp/app.js
+++ taskTrackerApp/app.js
@@ -18,6 +18,13 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
+// Make our db accessible to our router
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
+
+
 // app.use(favicon(dirname + '/public/favicon.ico'));
 app.use(logger("dev"));
 app.use(bodyParser.json());
@@ -25,11 +32,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Make our db accessible to our router
-app.use(function(req, res, next) {
-  req.db = db;
-  next();
-});
 
 app.use("/", routes);
 app.use("/users", users);
