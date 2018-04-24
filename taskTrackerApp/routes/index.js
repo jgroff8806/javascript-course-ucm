08ca8107c3e39d84bfd6557fae1b9b215222dfed
Jason <jgroff88@gmail.com>
2018-04-23 21:53:59 -0500

    Removed old code and added comments

--- taskTrackerApp/routes/index.js
+++ taskTrackerApp/routes/index.js
@@ -22,8 +22,6 @@ router.get("/tasklist", function(req, res) {
 
 // Remove Task
 router.post("/removetask/:id", function(req, res) {
-  console.log("wtf");
-  console.log(req.params.id);
   TaskModel.findByIdAndRemove(req.params.id, function(err, task) {
     if (err) {
       console.log(err);
@@ -45,7 +43,7 @@ router.get("/addtask", function(req, res) {
   res.render("addtask", { title: "Add New Task" });
 });
 
-// Update Task
+// Navigate to Update page
 router.get("/update/:id", function(req, res) {
   TaskModel.findById(req.params.id, function(err, task) {
     if (err) {
@@ -55,6 +53,7 @@ router.get("/update/:id", function(req, res) {
   });
 });
 
+// Update Task
 router.post("/updatetask/:id", function(req, res) {
   const id = req.params.id;
   const name = req.body.task;
@@ -72,13 +71,6 @@ router.post("/updatetask/:id", function(req, res) {
 
 // POST to Add Task Service
 router.post("/addtask", function(req, res) {
-  // Set our internal DB variable
-  // var db = req.db;
-
-  // Get our form values. These rely on the "name" attributes
-  //var taskName = req.body.taskname;
-  //var taskDesc = req.body.taskdesc;
-
   var newTask = new TaskModel({ task: req.body.taskname, description: req.body.taskdesc });
 
   newTask.save(function(err) {
@@ -88,26 +80,6 @@ router.post("/addtask", function(req, res) {
 
     res.redirect("tasklist");
   });
-
-  // Set our collection
-  // var collection = db.get("usercollection");
-
-  // Submit to the DB
-  // collection.insert(
-  //   {
-  //     task: taskName,
-  //     description: taskDesc
-  //   },
-  //   function(err, doc) {
-  //     if (err) {
-  //       // If it failed, return error
-  //       res.send("There was a problem adding the information to the database.");
-  //     } else {
-  //       // And forward to success page
-  //       res.redirect("tasklist");
-  //     }
-  //   }
-  // );
 });
 
 module.exports = router;
