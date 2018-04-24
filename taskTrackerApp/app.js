506d8eadd8dae8e396d3480e7da588050f780a43
Jason <jgroff88@gmail.com>
2018-04-24 01:35:30 -0500

    Added box-shadow to tasklist thumbnail, added gradient background to body

--- taskTrackerApp/app.js
+++ taskTrackerApp/app.js
@@ -6,10 +6,7 @@ var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-// New Code
 var mongo = require("mongodb");
-// var monk = require("monk");
-// var db = monk("localhost:27017/taskTrackerApp");
 
 mongoose
   .connect("mongodb://localhost:27017/taskTrackerApp")
@@ -21,15 +18,9 @@ var users = require("./routes/users");
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
-// Make our db accessible to our router
-// app.use(function(req, res, next) {
-//   req.db = db;
-//   next();
-// });
 
 app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(logger("dev"));
@@ -41,17 +32,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);
 app.use("/users", users);
 
-/// catch 404 and forwarding to error handler
+// Catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
+// Error Handlers
+// Development error handler will print stacktrace
 if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -62,8 +51,8 @@ if (app.get("env") === "development") {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler
+// No stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
